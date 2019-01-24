bf8c1a9fc4f32060b92a19aca0ef6be0e46bd26a
lintian <17210720082@fudan.edu.cn>
2019-01-24 15:20:11 +0800

    revision 1.24

--- load_samples.py
+++ load_samples.py
@@ -2,57 +2,23 @@ import numpy as np
 import scipy.io as sio
 
 
-def load_data_prefect_csi(flag):
-    # 0: Large sample sets for training
-    # 1: Small sample sets for testing
-    if flag == 0:
-        #  load the real and imag partition of the perfect CSI for training
-        #  150000 samples are used during the training
-        hr = sio.loadmat('./sample_set/h_real_train.mat')['Hr_nar']
-        print('load training hr: shape is', hr.shape)
-        hi = sio.loadmat('./sample_set/h_imag_train.mat')['Hi_nar']
-        print('load training hi: shape is', hi.shape)
-
-    if flag == 1:
-        #  load the real and imag partition of the perfect CSI for testing
-        #  10000 samples are used during the testing
-        hr = sio.loadmat('./sample_set/h_real_test.mat')['Hr_nar']
-        print('load testing hr: shape is', hr.shape)
-        hi = sio.loadmat('./sample_set/h_imag_test.mat')['Hi_nar']
-        print('load testing hi: shape is', hi.shape)
-
-    return hr, hi
+def save_mat(x):
+    sio.savemat('E:/WCL_MATLAB/DL_temp.mat', {'DL': x})
+    return 0
 
 
-def load_data_estimated_csi(flag):
-    # 0: Large sample sets for training
-    # 1: Small sample sets for testing
-    if flag == 0:
-        #  load the real and imag partition of the estimated CSI for training
-        #  150000 samples are used during the training
-        hr = sio.loadmat('./sample_set/h_real_train2.mat')['Hr_nar']
-        print('load training hr: shape is', hr.shape)
-        hi = sio.loadmat('./sample_set/h_imag_train2.mat')['Hi_nar']
-        print('load training hi: shape is', hi.shape)
-        hr_est = sio.loadmat('./sample_set/h_est_real_train.mat')['Hr_est']
-        print('load training estimated hr: shape is', hr_est.shape)
-        hi_est = sio.loadmat('./sample_set/h_est_imag_train.mat')['Hi_est']
-        print('load training estimated hi: shape is', hi_est.shape)
-        noise_power = sio.loadmat('./sample_set/Noise_power_train.mat')['Noise_power']
+def mat_load():
+    path = input('copy the path of package containing the samples in the python console to load data: \n')
+    hr = sio.loadmat(path + '/Hr_nar.mat')['Hr_nar']
+    print('load testing hr: shape is', hr.shape)
+    hi = sio.loadmat(path + '/Hi_nar.mat')['Hi_nar']
+    print('load testing hi: shape is', hi.shape)
+    hr_est = sio.loadmat(path + '/Hr_est.mat')['Hr_est']
+    print('load training estimated hr: shape is', hr_est.shape)
+    hi_est = sio.loadmat(path + '/Hi_est.mat')['Hi_est']
+    print('load training estimated hi: shape is', hi_est.shape)
+    return hr, hi, hr_est, hi_est
 
-    if flag == 1:
-        #  load the real and imag partition of the estimated CSI for testing
-        #  10000 samples are used during the testing
-        hr = sio.loadmat('./sample_set/h_real_test2.mat')['Hr_nar']
-        print('load testing hr: shape is', hr.shape)
-        hi = sio.loadmat('./sample_set/h_imag_test2.mat')['Hi_nar']
-        print('load testing hi: shape is', hi.shape)
-        hr_est = sio.loadmat('./sample_set/h_est_real_test.mat')['Hr_est']
-        print('load testing estimated hr: shape is', hr_est.shape)
-        hi_est = sio.loadmat('./sample_set/h_est_imag_test.mat')['Hi_est']
-        print('load testing estimated hi: shape is', hi_est.shape)
-        noise_power = sio.loadmat('./sample_set/Noise_power_test.mat')['Noise_power']
 
-    return hr, hi, hr_est, hi_est, noise_power
 
 
--- main_imperfect_CSI.py
+++ main_imperfect_CSI.py
@@ -13,38 +13,32 @@ Mode = int(input('Please type 0 or 1 in the python console to choose Mode: \n 0:
 
 if Mode:  # test mode
     Model_path = input('copy trained model path for testing: \n')  # copy the path of the trained NN to test
-    model = bfnn_model()  # construct the BFNN model
+    model = bfnn_model_est()  # construct the BFNN model
     model.load_weights(Model_path)  # load the trained weights
-    hr, hi, hr_est, hi_est, noise_power = load_data_estimated_csi(Mode)  # load the perfect CSI and estimated CSI
+    hr, hi, hr_est, hi_est = mat_load()  # load test samples
     sample_num, Nr = hr.shape  # numbers of samples and receive antennas
     BFNN_rate = []  # create a list to save rate results of different SNRs
-    for SNR in range(-20, 25, 5):
-        sigma_2 = 1 / np.power(10, SNR / 10)  # generate sigma^{-2}
-        # find the corresponding index of the estimated csi under this snr
-        Index = np.nonzero(noise_power == sigma_2)[0]
-        # find the samples under this snr
-        noise_power_snr = noise_power[Index]
-        hr_est_snr = hr_est[Index]
-        hi_est_snr = hi_est[Index]
-        hr_snr = hr[Index]
-        hi_snr = hi[Index]
-        signal_power = 1/ noise_power_snr  # generate the corresponding sigma^{-2}
-        #  the input_imperfect_info includes estimated csi and snr
-        input_imperfect_info = np.concatenate([hr_est_snr, hi_est_snr, signal_power], 1)
-        input_perfect_info = np.concatenate([hr_snr, hi_snr, signal_power], 1)
+    for SNR in range(-20, 25, 10):
+        signal_power = np.power(10, SNR / 10) * np.ones([sample_num, 1])  # generate the corresponding sigma^{-2}
+        input_perfect_info = np.concatenate([hr,hi, signal_power], 1)   # the dimension of the input is 2Nt+1
+        input_imperfect_info = np.concatenate([hr_est, hi_est, signal_power], 1)
         # v_RF = model.predict(input_imperfect_info)  # the phases of the obtained v_RF
-        # the imperfect csi (estimated by some traditional estimation method) is fed into the BFNN to form the vRF
-        # and then the perfect csi is fed into the loss function to compute the loss (the rate of the system)
+        # save_mat(v_RF)  #save the output as .mat file
+        # in this scenario that assume the perfect CSI is known at the transmitter, the input_info is fed into the
+        # BFNN to form the vRF, and then also fed into the loss function to compute the loss (the rate of the system)
         rate = model.evaluate(input_imperfect_info, input_perfect_info, batch_size=2000)
         BFNN_rate.append(-rate)  # save the result under this SNR
     print(BFNN_rate)
 
 else:  # train mode
-    hr, hi, hr_est, hi_est, noise_power = load_data_estimated_csi(Mode)  # load the perfect CSI and estimated CSI
+    hr, hi, hr_est, hi_est = mat_load()
     sample_num, Nr = hr.shape  # numbers of samples and receive antennas
-    model = bfnn_model()  # construct the BFNN model
-    # model.load_weights('./trained.h5') # start from trained model, for continued training or fine-tuning
-    signal_power = 1/ noise_power  # generate the corresponding sigma^{-2}
+    print('sample:', sample_num)
+    model = bfnn_model_est()  # construct the BFNN model
+    #model.load_weights('./trained_models/temp_imperfect_trained.h5') # start from trained model, for continued training or fine-tuning
+    #SNR = np.random.randint(-20, 20, [sample_num, 1])  # generate samples with different SNRs
+    SNR = 20 * np.ones([sample_num, 1])   # generate fixed SNRs
+    signal_power = np.power(10, SNR / 10)  # generate the corresponding sigma^{-2}
     input_imperfect_info = np.concatenate([hr_est, hi_est, signal_power], 1)
     input_perfect_info = np.concatenate([hr, hi, signal_power], 1)  # the dimension of the input is 2Nt+1
     # the checkpoint is for saving the model weights with best performance of the validation set
@@ -53,7 +47,7 @@ else:  # train mode
         save_best_only=True, mode='min',save_weights_only=True)
     # in this scenario that assume the perfect CSI is known at the transmitter, the input_info is fed into the
     # BFNN to form the vRF, and then also fed into the loss function to compute the loss (the rate of the system)
-    model.fit(input_imperfect_info, input_perfect_info, epochs=30000, batch_size=512, verbose=2,
+    model.fit(input_imperfect_info, input_perfect_info, epochs=30000, batch_size=8192, verbose=2,
               callbacks=[checkpoint], validation_split=0.2)
 
 
